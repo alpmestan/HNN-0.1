bef2cc645c262b4285eed03d181a028f8f6f216c
Joel Burget <joelburget@gmail.com>
2011-07-24 15:16:38 -0700

    Updated to use Vector

--- AI/HNN/Layer.hs
+++ AI/HNN/Layer.hs
@@ -1,68 +1,76 @@
--- | Layer module, defining functions to work on a neural network layer, which is a list of neurons
+-- | Layer module, defining functions to work on a neural network layer, which
+--   is a list of neurons
 module AI.HNN.Layer where
 
 import AI.HNN.Neuron
 import Control.Arrow
-import Data.Array.Vector
-import Data.List
+import Data.Vector (Vector(), fromList, sum, toList, zipWith)
+import Data.List (foldl')
 
 -- * Layer creation
 
--- | Creates a layer compound of n neurons with the Sigmoid transfer function, all having the given threshold and weights.
-createSigmoidLayerU :: Int -> Double -> UArr Double -> [Neuron]
+-- | Creates a layer compound of n neurons with the Sigmoid transfer function,
+--   all having the given threshold and weights.
+createSigmoidLayerU :: Int -> Double -> Vector Double -> [Neuron]
 createSigmoidLayerU n threshold weights = 
-             let neuron = createNeuronSigmoidU threshold weights in
-             take n . repeat $ neuron
+  take n . repeat $ neuron
+  where neuron = createNeuronSigmoidU threshold weights
 
--- | Creates a layer compound of n neurons with the Heavyside transfer function, all having the given threshold and weights.
-createHeavysideLayerU :: Int -> Double -> UArr Double -> [Neuron]
+-- | Creates a layer compound of n neurons with the Heavyside transfer
+--   function, all having the given threshold and weights.
+createHeavysideLayerU :: Int -> Double -> Vector Double -> [Neuron]
 createHeavysideLayerU n threshold weights =
-             let neuron = createNeuronSigmoidU threshold weights in 
-             take n . repeat $ neuron
+  take n . repeat $ neuron
+  where neuron = createNeuronSigmoidU threshold weights
 
--- | Creates a layer compound of n neurons with the sigmoid transfer function, all having the given threshold and weights.
+-- | Creates a layer compound of n neurons with the sigmoid transfer function,
+--   all having the given threshold and weights.
 createSigmoidLayer :: Int -> Double -> [Double] -> [Neuron]
-createSigmoidLayer n threshold = createSigmoidLayerU n threshold . toU
+createSigmoidLayer n threshold = createSigmoidLayerU n threshold . fromList
 
--- | Creates a layer compound of n neurons with the sigmoid transfer function, all having the given threshold and weights.
+-- | Creates a layer compound of n neurons with the sigmoid transfer function,
+--   all having the given threshold and weights.
 createHeavysideLayer :: Int -> Double -> [Double] -> [Neuron]
-createHeavysideLayer n threshold = createHeavysideLayerU n threshold . toU
+createHeavysideLayer n threshold = createHeavysideLayerU n threshold . fromList
 
 -- * Computation
 
 -- | Computes the outputs of each Neuron of the layer
-computeLayerU :: [Neuron] -> UArr Double -> UArr Double
-computeLayerU ns inputs = toU $ map (\n -> computeU n inputs) ns
+computeLayerU :: [Neuron] -> Vector Double -> Vector Double
+computeLayerU ns inputs = fromList $ map (\n -> computeU n inputs) ns
 
 -- | Computes the outputs of each Neuron of the layer
 computeLayer :: [Neuron] -> [Double] -> [Double]
-computeLayer ns = fromU . computeLayerU ns . toU
+computeLayer ns = toList . computeLayerU ns . fromList
 
 -- * Learning
 
 -- | Trains each neuron with the given sample and the given learning ratio
-learnSampleLayerU :: Double -> [Neuron] -> (UArr Double, UArr Double) -> [Neuron]
-learnSampleLayerU alpha ns (xs, ys) = zipWith (\n y -> learnSampleU alpha n (xs, y)) ns (fromU ys)
+learnSampleLayerU :: Double -> [Neuron] -> (Vector Double, Vector Double) -> [Neuron]
+learnSampleLayerU alpha ns (xs, ys) = 
+  Prelude.zipWith (\n y -> learnSampleU alpha n (xs, y)) ns $ toList ys
 
 -- | Trains each neuron with the given sample and the given learning ratio
 learnSampleLayer :: Double -> [Neuron] -> ([Double], [Double]) -> [Neuron]
-learnSampleLayer alpha ns = learnSampleLayerU alpha ns . (toU *** toU)
+learnSampleLayer alpha ns = learnSampleLayerU alpha ns . (fromList *** fromList)
 
 -- | Trains each neuron with the given samples and the given learning ratio
-learnSamplesLayerU :: Double -> [Neuron] -> [(UArr Double, UArr Double)] -> [Neuron]
-learnSamplesLayerU alpha = foldl' (learnSampleLayerU alpha)
+learnSamplesLayerU :: Double -> [Neuron] -> [(Vector Double, Vector Double)] -> [Neuron]
+learnSamplesLayerU alpha = Data.List.foldl' (learnSampleLayerU alpha)
 
 -- | Trains each neuron with the given samples and the given learning ratio
 learnSamplesLayer :: Double -> [Neuron] -> [([Double], [Double])] -> [Neuron]
-learnSamplesLayer alpha ns = learnSamplesLayerU alpha ns . map (toU *** toU)
+learnSamplesLayer alpha ns = 
+  learnSamplesLayerU alpha ns . map (fromList *** fromList)
 
 -- * Quadratic Error
 
 -- | Returns the quadratic error of a layer for a given sample
-quadErrorU :: [Neuron] -> (UArr Double, UArr Double) -> Double
-quadErrorU ns (xs, ys) = let os = computeLayerU ns xs
-                        in (/2) $ sumU $ zipWithU (\o y -> (y - o)**2) os ys
+quadErrorU :: [Neuron] -> (Vector Double, Vector Double) -> Double
+quadErrorU ns (xs, ys) = 
+  (/2) $ Data.Vector.sum $ Data.Vector.zipWith (\o y -> (y - o)**2) os ys
+  where os = computeLayerU ns xs
 
 -- | Returns the quadratic error of a layer for a given sample
 quadError :: [Neuron] -> ([Double], [Double]) -> Double
-quadError ns = quadErrorU ns . (toU *** toU)
+quadError ns = quadErrorU ns . (fromList *** fromList)
--- tests/xor-3inputs.hs
+++ tests/xor-3inputs.hs
@@ -1,27 +1,35 @@
 module Main where
-  
+
 import AI.HNN.Net
 import AI.HNN.Layer
 import AI.HNN.Neuron
-import Data.Array.Vector
 import Control.Arrow
 import Data.List
-  
-alpha = 0.8 :: Double -- learning ratio
-epsilon = 0.001 :: Double -- desired maximal bound for the quad error
+
+alpha, epsilon :: Double
+alpha = 0.8     -- learning ratio
+epsilon = 0.001 -- desired maximal bound for the quad error
 
 layer1, layer2 :: [Neuron]
-layer1 = map (createNeuronSigmoid 0.5 . toU) [[0.5, 0.5, 0.5], [0.5,0.5,0.5], [0.5,0.5,0.5], [0.5, 0.5, 0.5]]
-layer2 = [createNeuronSigmoid 0.5 $ toU [0.5, 0.4, 0.6, 0.3]]
- 
-net = [layer1, layer2]
-  
-finalnet = train alpha epsilon net . map (toU *** toU) $ [([1, 1, 1],[0]), ([1, 0, 1],[1]), ([1, 1, 0],[1]), ([1, 0, 0],[0])]
+layer1 = map (createNeuronSigmoid 0.5)
+  [[0.5, 0.5, 0.5],
+   [0.5, 0.5, 0.5],
+   [0.5, 0.5, 0.5],
+   [0.5, 0.5, 0.5]]
+layer2 = [createNeuronSigmoid 0.5 [0.5, 0.4, 0.6, 0.3]]
 
-good111 = computeNet finalnet $ toU [1, 1, 1]
-good101 = computeNet finalnet $ toU [1, 0, 1]
-good110 = computeNet finalnet $ toU [1, 1, 0]
-good100 = computeNet finalnet $ toU [1, 0, 0]
+net = [layer1, layer2]
+
+finalnet = train alpha epsilon net
+  [([1, 1, 1], [0]),
+   ([1, 0, 1], [1]),
+   ([1, 1, 0], [1]),
+   ([1, 0, 0], [0])]
+
+good111 = computeNet finalnet [1, 1, 1]
+good101 = computeNet finalnet [1, 0, 1]
+good110 = computeNet finalnet [1, 1, 0]
+good100 = computeNet finalnet [1, 0, 0]
 
 main = do
      putStrLn $ "Final neural network : \n" ++ show finalnet
